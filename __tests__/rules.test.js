6b140eea0de62444ff0438391b03f2ae0ecdb869
Andrey Mork <35698586+AndreyMork@users.noreply.github.com>
2019-10-14 16:18:03 +0300

    test: update tests

--- __tests__/rules.test.js
+++ __tests__/rules.test.js
@@ -5,7 +5,9 @@ import { RuleTester } from 'eslint';
 import type { RuleBundleType } from '../src/types/index.flow';
 import rules from '../src/rules';
 
-const ruleTester: RuleTester = new RuleTester({ parser: 'babel-eslint' });
+const ruleTester: RuleTester = new RuleTester({
+  parser: require.resolve('babel-eslint'),
+});
 
 rules.forEach(({ ruleName, ruleObject, assertions }: RuleBundleType) => {
   ruleTester.run(ruleName, ruleObject, assertions);
